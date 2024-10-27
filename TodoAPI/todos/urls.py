b55365245368110b9a1a2f84a45f56fe355435a8
Quang Nguyen <npquang14041999@gmail.com>
2024-10-27 21:24:36 +0700

    fix import url

--- TodoAPI/todos/urls.py
+++ TodoAPI/todos/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.urls import path
+
 from .views import UserViewSet,TodoViewSet
 
 user_list = UserViewSet.as_view({
